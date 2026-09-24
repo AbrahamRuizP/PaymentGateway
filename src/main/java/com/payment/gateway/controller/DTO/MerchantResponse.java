2532c5eb0c6bd489cb4ea767b6fc1a95dae6e831
abrorestes <habramruizpestana@gmail.com>
2026-09-24 18:06:13 -0300

    add Builder annotation to MerchantResponse for improved object creation

--- src/main/java/com/payment/gateway/controller/DTO/MerchantResponse.java
+++ src/main/java/com/payment/gateway/controller/DTO/MerchantResponse.java
@@ -1,10 +1,12 @@
 package com.payment.gateway.controller.DTO;
 
 import com.payment.gateway.entity.enums.MerchantStatus;
+import lombok.Builder;
 
 import java.time.Instant;
 import java.util.UUID;
 
+@Builder
 public record MerchantResponse(
         UUID id,
         String businessName,
