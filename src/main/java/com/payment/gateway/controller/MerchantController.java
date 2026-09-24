186890c5fe854362e5a0d12d92a704b42418ad41
abrorestes <habramruizpestana@gmail.com>
2026-09-24 12:15:56 -0300

    add create and retrieve merchant functionality in MerchantController and MerchantService

--- src/main/java/com/payment/gateway/controller/MerchantController.java
+++ src/main/java/com/payment/gateway/controller/MerchantController.java
@@ -1,14 +1,45 @@
 package com.payment.gateway.controller;
 
+import com.payment.gateway.controller.DTO.CreateMerchantRequest;
+import com.payment.gateway.controller.DTO.MerchantResponse;
+import com.payment.gateway.entity.Merchant;
+import com.payment.gateway.exception.MerchantNotFoundException;
+import com.payment.gateway.service.MerchantService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.UUID;
 
 @RequiredArgsConstructor
 @RestController
 @RequestMapping("/merchants")
 public class MerchantController {
 
+    private final MerchantService merchantService;
 
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public MerchantResponse createMerchant(@RequestBody @Valid CreateMerchantRequest request) {
+        Merchant m = merchantService.createMerchant(request);
+
+        return toResponse(m);
+    }
+
+    @GetMapping("/{id}")
+    @ResponseStatus(HttpStatus.OK)
+    public MerchantResponse getMerchant(@PathVariable UUID id) {
+        Merchant m = merchantService.findById(id)
+                .orElseThrow(() -> new MerchantNotFoundException(id));
+
+        return toResponse(m);
+    }
+
+    private static MerchantResponse toResponse(Merchant m) {
+        return new MerchantResponse(
+                m.getId(), m.getBusinessName(), m.getEmail(), m.getStatus(), m.getCreatedAt()
+        );
+    }
 
 }
--- src/main/java/com/payment/gateway/service/MerchantService.java
+++ src/main/java/com/payment/gateway/service/MerchantService.java
@@ -1,8 +1,14 @@
 package com.payment.gateway.service;
 
+import com.payment.gateway.controller.DTO.CreateMerchantRequest;
+import com.payment.gateway.entity.Merchant;
+import com.payment.gateway.entity.enums.MerchantStatus;
 import com.payment.gateway.repository.MerchantRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+import java.util.UUID;
 
 @Service
 @RequiredArgsConstructor
@@ -10,4 +16,20 @@ public class MerchantService {
 
     private final MerchantRepository merchantRepository;
 
+    public Optional<Merchant> findById(UUID id) {
+        return merchantRepository.findById(id);
+    }
+
+    public Merchant createMerchant(CreateMerchantRequest request) {
+        Merchant merchant = buildMerchant(request);
+        return merchantRepository.save(merchant);
+    }
+
+    private static Merchant buildMerchant(CreateMerchantRequest request) {
+        return Merchant.builder()
+                .email(request.email())
+                .businessName(request.businessName())
+                .status(MerchantStatus.CREATED)
+                .build();
+    }
 }
