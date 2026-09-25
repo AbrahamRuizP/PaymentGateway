2e70bc36e5f95881f26830fec0febf7a3ad38d95
abrorestes <habramruizpestana@gmail.com>
2026-09-25 17:11:06 -0300

    add validation for amount in CreatePaymentIntentRequest

--- src/main/java/com/payment/gateway/controller/DTO/CreatePaymentIntentRequest.java
+++ src/main/java/com/payment/gateway/controller/DTO/CreatePaymentIntentRequest.java
@@ -2,12 +2,14 @@ package com.payment.gateway.controller.DTO;
 
 import com.payment.gateway.entity.enums.Currency;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
 
 import java.math.BigDecimal;
 import java.util.UUID;
 
 public record CreatePaymentIntentRequest (
         @NotNull
+        @Positive
         BigDecimal amount,
 
         @NotNull
